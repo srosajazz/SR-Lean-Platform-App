487e5279536eaa7f8c7bf0b26cde195ac357570b
Sergio Rosa <scarlosjazz@gmail.com>
2021-11-21 07:59:02 -0500

    backend project created

---
+++ backend/srlearn/src/test/java/com/sergiorosa/SrlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
