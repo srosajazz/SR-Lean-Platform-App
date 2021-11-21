c86c4ac344816fb2bb1fea76c3601a848f7c8f29
Sergio Rosa <scarlosjazz@gmail.com>
2021-11-21 12:24:56 -0500

    add: Course

---
+++ workplace/SR-Lean-Platform-App-main/backend/srlearn/src/main/java/com/sergiorosa/SrlearnApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrlearnApplication.class, args);
+	}
+
+}
---
+++ workplace/SR-Lean-Platform-App-main/backend/srlearn/src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Ensemble Courses- ENEL-304','https://live.staticflickr.com/2816/33450300753_10fbe16d08_b.jpg','https://upload.wikimedia.org/wikipedia/commons/c/ce/%C3%98ystein_Skjelstad_%C3%98stensen_Oslo_Jazzfestival_2020_%28150950%29.jpg');
